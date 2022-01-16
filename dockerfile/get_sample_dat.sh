2d565600e288e89cc4335d7131299d8ba2634258
BilliBag2 <bagshawwilliam@gmail.com>
2022-01-16 23:10:38 +0000

    Improvments to docker file. Added a script to get the samples. and a scrit to run docker container.

---
+++ dockerfile/get_sample_dat.sh
@@ -0,0 +1,3 @@
+gdown --id 18JxhpWD-4ZmuFKLzKlAw-w5PpzZxXOcG
+gdown --id 1jzggQ7IPaJJTKx9yLASWHrX8dXHnG5eB
+gdown --id 1PG-KllCv4vSRPO7n5lpBjyTjlUyT8Nag
---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+docker run -it --name phenoxels billybag2/phenoxels
